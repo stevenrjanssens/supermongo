de776a0cad02095ed6b49827749b37c3182cabd9
Steven Janssens <steven.janssens@utoronto.ca>
2024-03-17 15:53:23 -0600

    pointed to MongoDB Atlas database

--- src/supermongo/database.py
+++ src/supermongo/database.py
@@ -1,9 +1,14 @@
 """database.py -- tools to interface with MongoDB"""
 
-from pymongo import MongoClient
+import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 def _get_database():
-    client = MongoClient("localhost", 27017)
+    mongocloud_user = os.getenv('MONGOCLOUD_USER')
+    mongocloud_pass = os.getenv('MONGOCLOUD_PASS')
+    uri = f"mongodb+srv://{mongocloud_user}:{mongocloud_pass}@cluster0.sfb5zzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    client = MongoClient(uri, server_api=ServerApi('1'))
     return client['dfimages']
 
 headers_collection = _get_database()['headers']
